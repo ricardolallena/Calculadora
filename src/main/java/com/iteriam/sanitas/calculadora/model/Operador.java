20893388171cdc93db75e79de0e3e282d0b8e0c1
ricardolallena <lallena.ricardo@gmail.com>
2023-02-21 20:51:03 +0100

    enum para definir los operadores que acepta el microservicio

---
+++ src/main/java/com/iteriam/sanitas/calculadora/model/Operador.java
@@ -0,0 +1,9 @@
+package com.iteriam.sanitas.calculadora.model;
+
+/**
+ * Enumeracion de operaciones aritmeticas disponibles. 
+ * Valida el campo operador en la peticiones.
+ */
+public enum Operador {
+    SUMA,RESTA;
+}
