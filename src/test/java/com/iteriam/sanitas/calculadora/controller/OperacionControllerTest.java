73c6655dd2ec1501e9754b75e884804cfef48ce2
ricardolallena <lallena.ricardo@gmail.com>
2023-02-21 21:24:35 +0100

    test del controlador operacion

---
+++ src/test/java/com/iteriam/sanitas/calculadora/controller/OperacionControllerTest.java
@@ -0,0 +1,25 @@
+package com.iteriam.sanitas.calculadora.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import com.iteriam.sanitas.calculadora.model.Operador;
+
+/**
+ * test del controlador de la operacion
+ */
+@SpringBootTest
+class OperacionControllerTest {
+    
+    @Autowired
+    private OperacionCotrollerImpl operacionCotrollerImpl;
+
+    @Test
+    void operacionCotrollerImplTest() {
+        Assertions.assertInstanceOf(ResponseEntity.class, operacionCotrollerImpl.operacion("1", Operador.SUMA, "2"));
+    }
+
+}
