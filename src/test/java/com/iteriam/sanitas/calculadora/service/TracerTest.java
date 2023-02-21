d867abafb74e3e4c74a5d665cc108b71144dc76e
ricardolallena <lallena.ricardo@gmail.com>
2023-02-21 21:26:55 +0100

    test servicio tracer

---
+++ src/test/java/com/iteriam/sanitas/calculadora/service/TracerTest.java
@@ -0,0 +1,59 @@
+package com.iteriam.sanitas.calculadora.service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.iteriam.sanitas.calculadora.model.ErrorResponse;
+import com.iteriam.sanitas.calculadora.model.Operador;
+import com.iteriam.sanitas.calculadora.model.Resultado;
+import com.iteriam.sanitas.calculadora.model.TraceOperacion;
+import com.iteriam.sanitas.calculadora.service.operaciones.OperacionSuma;
+
+@SpringBootTest
+class TracerTest {
+
+    @Autowired
+    private Tracer tracer;
+
+    /**
+     * Test tracer String normal
+     */
+    @Test
+    void tracerStringTest() {
+        Assertions.assertTrue(tracer.trace("Prueba tracer"), "Error trace");
+    }
+
+    /**
+     * Test tracer modelo TracerOperacion
+     */
+    @Test
+    void tracerTracerOperacionTest() {
+        Assertions.assertTrue(tracer.trace(
+                new TraceOperacion(new Resultado(new BigDecimal("3.6")), new OperacionSuma("8", Operador.SUMA, "6.2"))),
+                "Error trace");
+    }
+
+    /**
+     * Test tracer modelo ErrorResponse
+     */
+    @Test
+    void tracerErrorResponseTest() {
+        Assertions.assertTrue(
+                tracer.trace(new ErrorResponse(0, new Date(), "Prueba tracer", "Prueba tracer descripcion")),
+                "Error trace");
+    }
+
+    /**
+     * Test tracer catch Exception
+     */
+    @Test
+    void tracerCatchTest() {
+        Assertions.assertFalse(tracer.trace(null), "Error test catch");
+    }
+
+}
