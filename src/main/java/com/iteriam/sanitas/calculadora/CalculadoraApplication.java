de61acdd316951fe08c123cd9c7ba7b0c37c75a0
ricardolallena <lallena.ricardo@gmail.com>
2023-02-21 20:28:37 +0100

    carga el contexto de springboot

---
+++ src/main/java/com/iteriam/sanitas/calculadora/CalculadoraApplication.java
@@ -0,0 +1,14 @@
+package com.iteriam.sanitas.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		//carga contexto de springboot
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+
+}
