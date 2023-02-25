294e4ab9fc238c64e4241cf601fb11d9da78b286
ricardolallena <lallena.ricardo@gmail.com>
2023-02-25 12:15:12 +0100

    Se modifica el sericio para poder agregar nuevas operaciones sin tener que modificar el servicio. Cumpliendo asi con el principio de principio Open-Closed

--- src/main/java/com/iteriam/sanitas/calculadora/service/OperacionServiceImpl.java
+++ src/main/java/com/iteriam/sanitas/calculadora/service/OperacionServiceImpl.java
@@ -1,5 +1,8 @@
 package com.iteriam.sanitas.calculadora.service;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +10,8 @@ import com.iteriam.sanitas.calculadora.model.Operador;
 import com.iteriam.sanitas.calculadora.model.Resultado;
 import com.iteriam.sanitas.calculadora.model.TraceOperacion;
 import com.iteriam.sanitas.calculadora.service.operaciones.Operacion;
-import com.iteriam.sanitas.calculadora.service.operaciones.OperacionResta;
-import com.iteriam.sanitas.calculadora.service.operaciones.OperacionSuma;
+
+import jakarta.annotation.PostConstruct;
 
 /**
  * implementacion del servicio operacion
@@ -18,21 +21,39 @@ public class OperacionServiceImpl implements OperacionService {
 
     @Autowired
     private Tracer tracer;
-    
+
+    @Autowired
+    private Map<String, Operacion> operaciones;
+
+    private Map<String, Operacion> operacionesCaseIinsensitiv;
+
+    /**
+     * Crea un mapa de operaciones sin case sensitive en get y put, para poder
+     * obtener concatenar "operacion" + operador
+     */
+    @PostConstruct
+    public void getOperacionesCaseIinsensitiv() {
+        this.operacionesCaseIinsensitiv = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        operacionesCaseIinsensitiv.putAll(this.operaciones);
+    }
+
     /**
      * obtiene, ejecuta y crea la traza de la operacion
      * Devuelve el resultado
      */
     @Override
     public Resultado execute(String num1, Operador operador, String num2) {
+
         Operacion operacion = this.getOperacion(num1, operador, num2);
-        Resultado resultado = operacion.getReultado();
+        Resultado resultado = operacion.getResultado();
         this.tracer.trace(new TraceOperacion(resultado, operacion));
         return resultado;
     }
+
     /**
      * Devuelve la operacion a ejecutar.
      * Si no existe el operador genera la excepcion IllegalArgumentException
+     * 
      * @param num1
      * @param operador
      * @param num2
@@ -42,10 +63,12 @@ public class OperacionServiceImpl implements OperacionService {
     private Operacion getOperacion(String num1, Operador operador, String num2) {
         Operacion operacion = null;
 
-        if (Operador.SUMA.equals(operador)) {
-            operacion = new OperacionSuma(num1, operador, num2);
-        } else if (Operador.RESTA.equals(operador)) {
-            operacion = new OperacionResta(num1, operador, num2);
+        if (operador != null) {
+            operacion = this.operacionesCaseIinsensitiv.get("operacion" + operador.name());
+        }
+
+        if (operacion != null) {
+            operacion.setParametros(num1, operador, num2);
         } else {
             throw new IllegalArgumentException("operador incorrecto.");
         }
