85a49bb22c44c31e8df8b3b13c339449ba5267fe
ricardolallena <lallena.ricardo@gmail.com>
2023-02-25 12:18:35 +0100

    se refactoriza la operacion para extender de OperacionComun y asi reutilizar el codigo

--- src/main/java/com/iteriam/sanitas/calculadora/service/operaciones/OperacionResta.java
+++ src/main/java/com/iteriam/sanitas/calculadora/service/operaciones/OperacionResta.java
@@ -1,50 +1,18 @@
 package com.iteriam.sanitas.calculadora.service.operaciones;
 
-import java.math.BigDecimal;
+import org.springframework.stereotype.Component;
 
-import com.iteriam.sanitas.calculadora.model.Operador;
 import com.iteriam.sanitas.calculadora.model.Resultado;
-
 
 /**
  * Clase para realizar la operacion resta
  */
-public class OperacionResta implements Operacion {
-
-    private BigDecimal num1;
-    private Operador operador;
-    private BigDecimal num2;
-
-    public OperacionResta(String num1, Operador operador, String num2) {
-        this.num1 = new BigDecimal(num1);
-        this.operador = operador;
-        this.num2 = new BigDecimal(num2);
-    }
+@Component
+public class OperacionResta extends OperacionComun implements Operacion {
 
     @Override
-    public Resultado getReultado() {
-        return new Resultado(this.num1.subtract(num2));
-    }
-
-    public BigDecimal getNum1() {
-        return this.num1;
-    }
-
-    public BigDecimal getNum2() {
-        return this.num2;
-    }
-
-    public Operador getOperador() {
-        return this.operador;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                " num1='" + getNum1() + "'" +
-                ", operador='" + getOperador() + "'" +
-                ", num2='" + getNum2() + "'" +
-                "}";
+    public Resultado getResultado() {
+        return new Resultado(this.getNum1().subtract(this.getNum2()));
     }
 
 }
--- src/main/java/com/iteriam/sanitas/calculadora/service/operaciones/OperacionSuma.java
+++ src/main/java/com/iteriam/sanitas/calculadora/service/operaciones/OperacionSuma.java
@@ -1,48 +1,18 @@
 package com.iteriam.sanitas.calculadora.service.operaciones;
 
-import java.math.BigDecimal;
+import org.springframework.stereotype.Component;
 
-import com.iteriam.sanitas.calculadora.model.Operador;
 import com.iteriam.sanitas.calculadora.model.Resultado;
 
 /**
- *  realizar la operacion suma
+ * realizar la operacion suma
  */
-public class OperacionSuma implements Operacion {
-
-    private BigDecimal num1;
-    private Operador operador;
-    private BigDecimal num2;
-
-    public OperacionSuma(String num1, Operador operador, String num2) {
-        this.num1 = new BigDecimal(num1);
-        this.operador = operador;
-        this.num2 = new BigDecimal(num2);
-    }
+@Component
+public class OperacionSuma extends OperacionComun implements Operacion {
 
     @Override
-    public Resultado getReultado() {
-        return new Resultado(this.num1.add(num2));
+    public Resultado getResultado() {
+        return new Resultado(this.getNum1().add(this.getNum2()));
     }
 
-    public BigDecimal getNum1() {
-        return this.num1;
-    }
-
-    public BigDecimal getNum2() {
-        return this.num2;
-    }
-
-    public Operador getOperador() {
-        return this.operador;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                " num1='" + getNum1() + "'" +
-                ", operador='" + getOperador() + "'" +
-                ", num2='" + getNum2() + "'" +
-                "}";
-    }
 }
